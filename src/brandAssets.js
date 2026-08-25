79cbfab8286b6f91f1c2c8bfa694f33e4bbf7046
Dominick “Nick” Mascolo <99149066+DCMedic@users.noreply.github.com>
2026-08-25 16:43:48 -0500

    Add original BattleReef brand asset references

---
+++ src/brandAssets.js
@@ -0,0 +1,5 @@
+export const originalBrandAssets = {
+  fullLogo: '/brand/original/battlereef-full.webp',
+  wordmark: '/brand/original/battlereef-wordmark.webp',
+  mark: '/brand/original/battlereef-mark.webp',
+}
