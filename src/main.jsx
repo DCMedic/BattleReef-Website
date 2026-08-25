c257db4c1ecc1f68106b2771b00b40742358a6cb
Dominick “Nick” Mascolo <99149066+DCMedic@users.noreply.github.com>
2026-08-25 17:13:12 -0500

    Load visible original brand treatment

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import App from './App.jsx'
 import './styles.css'
 import './expansion.css'
 import './brand-refinement.css'
+import './visual-hotfix.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
