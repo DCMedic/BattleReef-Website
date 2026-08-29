d2124c8dc6728af3ca922cc23ebf4dc71e760348
Dominick “Nick” Mascolo <99149066+DCMedic@users.noreply.github.com>
2026-08-28 19:08:02 -0500

    Remove obsolete brand asset module

--- src/brandAssets.js
+++
@@ -1,5 +0,0 @@
-export const originalBrandAssets = {
-  fullLogo: '/brand/original/battlereef-full.webp',
-  wordmark: '/brand/original/battlereef-wordmark.webp',
-  mark: '/brand/original/battlereef-mark.webp',
-}
